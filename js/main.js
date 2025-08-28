6b011b199ce54e80fbcb88442c96f1ddde5fcf48
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-08-28 10:44:09 +0300

    Add displayController module

--- js/main.js
+++ js/main.js
@@ -32,27 +32,39 @@ const library = (function(){
     return {addBookToLibrary, getLibraryContent};
 })();
 
-library.addBookToLibrary('Head First Java', 'Kathy Sierra  &  Bert Bates', 688, true);
-library.addBookToLibrary('Clean Code', 'Robert Martin', 464, false);
-library.addBookToLibrary('Java All-in-One For Dummies', 'Doug Lowe ', 912, false);
+const displayController = (function(){
 
+    const start = ()=>{
+        displayBooks();
+        configureAddBookDialog();
+    }
 
-function displayBooks(){
+    const displayBooks = () =>{
+        const libraryContainer = document.querySelector('.library');
+        libraryContainer.textContent = '';
+        const libraryContent = library.getLibraryContent();
+        for(let item of libraryContent){
+            const book = document.createElement('div');
+            book.classList.add('book');
+            
+            const title = document.createElement('h2');
+            title.textContent = item.title;
+            book.appendChild(title);
+    
+            const bookInfo = createBookInfoList(item);
+            book.appendChild(bookInfo);
 
-    const libraryContainer = document.querySelector('.library');
-    libraryContainer.textContent = '';
-    const libraryContent = library.getLibraryContent();
-    for(let item of libraryContent){
-        const book = document.createElement('div');
-        book.classList.add('book');
-        
-        const title = document.createElement('h2');
-        title.textContent = item.title;
-        book.appendChild(title);
+            const buttons = createBookButtons(item, libraryContent);
+            book.appendChild(buttons);
 
+            libraryContainer.appendChild(book);
+        }
+    }
+
+    const createBookInfoList = (book)=>{
         const bookInfo = document.createElement('ul');
         bookInfo.classList.add('book-info');
-        for(let prop in item){
+        for(let prop in book){
             if(prop === 'author' || prop === 'pages' || prop === 'read'){
                 let li = document.createElement('li');
                 let span = document.createElement('span');
@@ -60,70 +72,75 @@ function displayBooks(){
                 li.appendChild(span);
                 if(prop === 'read'){  
                     span.textContent = 'status: ' 
-                    let text = document.createTextNode((item.read?'read':'not read yet'));
+                    let text = document.createTextNode((book.read?'read':'not read yet'));
                     li.appendChild(text); 
                 }else{
-                    let text = document.createTextNode(item[prop]);
+                    let text = document.createTextNode(book[prop]);
                     li.appendChild(text); 
                 }
                 bookInfo.appendChild(li);
             }
-        }   
+        }  
+        return bookInfo;
+    }
 
-        book.appendChild(bookInfo);
-        const div = document.createElement('div');
-        div.classList.add('buttons');
+    const createBookButtons = (book, libraryContent)=>{
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.classList.add('buttons');
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'remove';
         removeBtn.classList.add('remove-btn');
-        removeBtn.dataset.id = item.id;
+        removeBtn.dataset.id = book.id;
         removeBtn.addEventListener('click',(e)=> {
-            libraryContent.splice(libraryContent.indexOf(item), 1);
+            libraryContent.splice(libraryContent.indexOf(book), 1);
             displayBooks();
         }); 
-        div.appendChild(removeBtn);
+        buttonsContainer.appendChild(removeBtn);
 
         const changeStatusBtn = document.createElement('button');
         changeStatusBtn.textContent = 'change status';
         changeStatusBtn.classList.add('change-status-btn');
-        changeStatusBtn.dataset.id = item.id;
+        changeStatusBtn.dataset.id = book.id;
         changeStatusBtn.addEventListener('click',(e)=> {
-            item.changeStatus();
+            book.changeStatus();
             displayBooks();
         }); 
-        div.appendChild(changeStatusBtn);
+        buttonsContainer.appendChild(changeStatusBtn);
 
-        book.appendChild(div);
-        libraryContainer.appendChild(book);
+        return buttonsContainer;
+    };
+
+    const configureAddBookDialog = ()=>{
+        const dialog = document.querySelector('dialog');
+        const showBtn = document.querySelector('.show-btn');
+        showBtn.addEventListener('click', (e)=>{
+            dialog.showModal()
+        });
+
+        const closeBtn = document.querySelector('.close-btn');
+        closeBtn.addEventListener('click', (e)=>{
+            dialog.close();
+        });
+
+        const addBtn = document.querySelector('.submit-btn');
+        addBtn.addEventListener('click', (e)=>{
+            e.preventDefault();
+            const titleInput = document.querySelector("input[name='title']");
+            const authorInput = document.querySelector("input[name='author']");
+            const pagesInput = document.querySelector("input[name='pages']");
+            const statusInput = document.querySelector("input[name='status']:checked"); 
+            library.addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, (statusInput.value === 'true'));
+            dialog.close();
+            displayBooks();
+        });
     }
-}
 
-displayBooks();
+    return {start};
+})();
 
-// dialog logic
-const dialog = document.querySelector('dialog');
-//show dialog btn
-const showBtn = document.querySelector('.show-btn');
-showBtn.addEventListener('click', (e)=>{
-    dialog.showModal()
-});
+library.addBookToLibrary('Head First Java', 'Kathy Sierra  &  Bert Bates', 688, true);
+library.addBookToLibrary('Clean Code', 'Robert Martin', 464, false);
+library.addBookToLibrary('Java All-in-One For Dummies', 'Doug Lowe ', 912, false);
 
-//close dialog btn
-const closeBtn = document.querySelector('.close-btn');
-closeBtn.addEventListener('click', (e)=>{
-    dialog.close();
-});
+displayController.start();
 
-// add book btn
-const titleInput = document.querySelector("input[name='title']");
-const authorInput = document.querySelector("input[name='author']");
-const pagesInput = document.querySelector("input[name='pages']");
-
-const addBtn = document.querySelector('.submit-btn');
-addBtn.addEventListener('click', (e)=>{
-    e.preventDefault();
-    const statusInput = document.querySelector("input[name='status']:checked"); 
-    library.addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, (statusInput.value === 'true'));
-    dialog.close();
-    displayBooks();
-});
